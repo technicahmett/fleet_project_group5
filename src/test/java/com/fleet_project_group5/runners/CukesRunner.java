808d538c3c38c730c1efa0c96a026af0bde05965
technicahmett <technicahmett@gmail.com>
2025-02-25 15:23:03 +0300

    used a new annotation for tag

--- src/test/java/com/fleet_project_group5/runners/CukesRunner.java
+++ src/test/java/com/fleet_project_group5/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         glue = "com/fleet_project_group5/step_definitions",
         dryRun = false,
 
-        tags = "@storeManager06"
+        tags = "@wip06"
 
        // publish = true //generating a report with public link
 )
