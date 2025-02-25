2cea470230882eb0d1d6db907f201b2a9ee67b94
technicahmett <technicahmett@gmail.com>
2025-02-25 15:45:55 +0300

    Added my own Runner files

---
+++ src/test/java/com/fleet_project_group5/runners/US06Runner.java
@@ -0,0 +1,26 @@
+package com.fleet_project_group5.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/fleet_project_group5/step_definitions",
+        dryRun = false,
+
+        tags="@driver"
+      //  tags="@salesManager06"
+        //tags = "@storeManager06"
+
+
+        // publish = true //generating a report with public link
+)
+
+public class US06Runner {}
